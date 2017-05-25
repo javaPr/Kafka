44bd22c107f32deefbf260f483a68bdfd257f434
wdchang <13changice@tongji.edu.cn>
2017-05-25 23:37:00 +0800

    write result into .txt file

---
+++ src/main/java/cn/edu/wang/Utils.java
@@ -0,0 +1,34 @@
+package cn.edu.wang;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+/**
+ * Created by wangdechang on 2017/5/25.
+ */
+public class Utils {
+    public static void write(String data,boolean newLine){
+        //String data = " This content will append to the end of the file";
+        try{
+            File file =new File("result.txt");
+
+            //if file doesnt exists, then create it
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            //true = append file
+            FileWriter fileWritter = new FileWriter(file.getName(),true);
+            BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+            if (newLine)bufferWritter.write("\r\n");
+            bufferWritter.write(data);
+            bufferWritter.close();
+
+        }catch (Exception e){
+
+        }
+
+
+    }
+}
